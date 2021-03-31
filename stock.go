77f9d6aa13e0ea7c13bcc6ee0637d0158f4da24e
LordNature <nature@galaxy.moe>
2021-03-30 20:56:56 -0500

    stock.go: Renamed struct `stock` to `quote` for database structs.

--- stock.go
+++ stock.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-type stock struct {
+type quote struct {
 	currPrice float64
 	prevClose float64
 }
@@ -34,32 +34,32 @@ func fetch(url string) ([]byte, error) {
 	return bytes, nil
 }
 
-func getStockQuote(symbol string) (stock, error) {
-	var s stock
+func getStockQuote(symbol string) (quote, error) {
+	var q quote
 
 	resp, err := fetch("https://query2.finance.yahoo.com/v10/finance/quoteSummary/" +
 		symbol + "?formatted=false&modules=price")
 	if err != nil {
-		return s, err
+		return q, err
 	}
 
 	// get current stock price
 	re := regexp.MustCompile(`regularMarketPrice\":[0-9]*\.[0-9]+`)
 	currPrice := string(re.Find(resp))
 	currPrice = strings.TrimPrefix(currPrice, "regularMarketPrice\":")
-	s.currPrice, err = strconv.ParseFloat(currPrice, 64)
+	q.currPrice, err = strconv.ParseFloat(currPrice, 64)
 	if err != nil {
-		return s, err
+		return q, err
 	}
 
 	// get previous close
 	re = regexp.MustCompile(`regularMarketPreviousClose\":[0-9]*\.[0-9]+`)
 	prevClose := string(re.Find(resp))
 	prevClose = strings.TrimPrefix(prevClose, "regularMarketPreviousClose\":")
-	s.prevClose, err = strconv.ParseFloat(prevClose, 64)
+	q.prevClose, err = strconv.ParseFloat(prevClose, 64)
 	if err != nil {
-		return s, err
+		return q, err
 	}
 
-	return s, nil
+	return q, nil
 }
