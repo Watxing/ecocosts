210ee1aef29ea6edef4482e7ceb9ea1696d68f10
LordNature <nature@galaxy.moe>
2021-04-02 16:50:29 -0500

    stock: Exported all data for templates to use.

--- stock.go
+++ stock.go
@@ -10,18 +10,18 @@ import (
 )
 
 type stock struct {
-	symbol    string
-	quantity  int
-	price     quote
+	Symbol   string
+	Quantity int
+	Price    quote
 }
 
 type quote struct {
-	currPrice float64
-	prevClose float64
+	CurrPrice float64
+	PrevClose float64
 }
 
 func (s *stock) getPrice() error {
-	if err := s.price.update(s.symbol); err != nil {
+	if err := s.Price.update(s.Symbol); err != nil {
 		return err
 	}
 
@@ -59,7 +59,7 @@ func (q *quote) update(symbol string) error {
 	re := regexp.MustCompile(`regularMarketPrice\":[0-9]*\.[0-9]+`)
 	currPrice := string(re.Find(resp))
 	currPrice = strings.TrimPrefix(currPrice, "regularMarketPrice\":")
-	q.currPrice, err = strconv.ParseFloat(currPrice, 64)
+	q.CurrPrice, err = strconv.ParseFloat(currPrice, 64)
 	if err != nil {
 		return err
 	}
@@ -68,7 +68,7 @@ func (q *quote) update(symbol string) error {
 	re = regexp.MustCompile(`regularMarketPreviousClose\":[0-9]*\.[0-9]+`)
 	prevClose := string(re.Find(resp))
 	prevClose = strings.TrimPrefix(prevClose, "regularMarketPreviousClose\":")
-	q.prevClose, err = strconv.ParseFloat(prevClose, 64)
+	q.PrevClose, err = strconv.ParseFloat(prevClose, 64)
 	if err != nil {
 		return err
 	}
