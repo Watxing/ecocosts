46918f59c54c022ab34fd1fef81fde909154324b
LordNature <nature@galaxy.moe>
2021-04-12 02:21:25 -0500

    ledger: Handle creation of ledgers and display 1000 transactions.

--- ledger.go
+++ ledger.go
@@ -2,39 +2,77 @@ package main
 
 import (
 	"net/http"
+	"database/sql"
+	"strconv"
+	"time"
 )
 
-func viewTransaction() ([]transaction, error) {
-	var t []transaction
-	rows, err := db.Query(`SELECT id, client_id, cat_id, amount, balance,
-		description, time FROM transaction`)
-	if err != nil {
-		return t, err
-	}
-
-	for rows.Next() {
-		var r transaction
-		err := rows.Scan(&r.ID, &r.Client_id, &r.Cat_id, &r.Amount, &r.Balance,
-			&r.Description, &r.Time)
-		if err != nil {
-			return t, err
-		}
-		t = append(t, r)
-	}
-
-	if err = rows.Err(); err != nil {
-		return t, err
-	}
-
-	return t, nil
-}
-
 func ledgerHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := viewTransaction()
+	var c client
+	if err := c.readCookie(w, r); err != nil {
+		http.Redirect(w, r, "/auth", http.StatusFound)
+		return
+	}
+
+	if r.Method == "POST" {
+		var t transaction
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		amt, err := strconv.ParseFloat(r.FormValue("amount"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Amount = amt
+
+		desc := r.FormValue("description")
+		if desc == "" {
+			t.Description = sql.NullString{"", false}
+		} else {
+			t.Description = sql.NullString{desc, true}
+		}
+
+		category, err := strconv.ParseInt(r.FormValue("category"), 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if category == 0 {
+			t.Cat_id = sql.NullInt64{0, false}
+		} else {
+			t.Cat_id = sql.NullInt64{category, true}
+		}
+
+		t.Client_id = c.id
+		t.Time = time.Now()
+		if err := t.insert(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if err := c.updateTransactions(1000); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	categories, err := getCategories();
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	if err := templates.ExecuteTemplate(w, "ledger.html", t); err != nil {
+
+	data := map[string]interface{} {
+		"Client": c,
+		"Categories": categories,
+	}
+
+	if err := templates.ExecuteTemplate(w, "ledger.html", data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
